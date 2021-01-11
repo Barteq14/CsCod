62477e90884e88a3ac4f2ff75661209a8f677541
denied323 <tmielcarz@tlen.pl>
2021-01-11 15:44:29 +0100

    Sprawdzanie czy nazwa funkcji i zmiennej prawidłowe.

--- ProjektSSIW/Form1.cs
+++ ProjektSSIW/Form1.cs
@@ -17,6 +17,8 @@ namespace ProjektSSIW
         public delegate void Delegat(string phrase);
         Składnia składnia = new Składnia();
         Zmienne zmienne = new Zmienne();
+        Funkcje funkcje = new Funkcje();
+
         public Form1()
         {
             InitializeComponent();
@@ -75,6 +77,124 @@ namespace ProjektSSIW
                         //then dodajemy
                     }
                     */
+
+
+
+
+                    //Funkcje sprawdzanie czy jest tylko 1 ciąg w linijce
+                    if (subs.Length == 1) //
+                    {
+                        char[] char_arr = subs[0].ToCharArray();//zamieniam stringa na tablicę charów
+                        if (char_arr[char_arr.Length-1] == ')') // jeżeli ostatni znak w tym stringu to )
+                        {
+                            bool test1 = false;//flaga czy jest (
+                            bool test2 = false;//flaga czy jest )
+                            bool test3 = false;//sprawdź czy najpierw jest ( a później )
+                            String pomocniczaIndexNazwaTypu=""; //nazwa operacji funkcji przed ( np. jak będzie ak47(test) no to tutaj będzie ak47
+                            String pomocniczaIndexNazwaZmiennej = ""; // nazwa zmiennej funkcji
+                            foreach (var item in char_arr) //sprawdzam wszystkie znaki tego stringa
+                            {
+                                if (item == '(') // flaga (
+                                {
+                                    test1 = true;
+                                }
+                                if (item == ')') //flaga )
+                                {
+                                    test2 = true;
+                                }
+                                if (test1 == false && test2 == false) //wpisuje do stringa jaka operacje funkcji
+                                {
+                                    pomocniczaIndexNazwaTypu = pomocniczaIndexNazwaTypu + item;
+                                }
+                                if (test2 == true && test1 == false) // jeżeli najpierw znajdzie w ciągu jest ) a później ( to błąd
+                                {
+                                    test3 = false;
+                                    label4.Text = "blad1";
+                                    break;
+                                }
+                                if (test1 == true && test2== false) // wpisuje do stringa nazwę funkcji
+                                {
+                                    if (item != '(') //ten if żeby nie wypisywało "(nazwa" tylko samo "nazwa"
+                                    {
+                                        pomocniczaIndexNazwaZmiennej = pomocniczaIndexNazwaZmiennej + item;
+                                    }
+                                }
+                                if (test1 == true && test2 == true) // jeżeli wszystko ok to jest flaga ustawiana
+                                {
+                                    test3 = true;
+                                    break;
+                                }
+                                
+                            }
+                            if (test3 != true)
+                            {
+                                label4.Text = "Źle wpisana nazwa zmiennej funkcji"; // komunikat
+                            }
+                            else
+                            {
+                                Boolean czyGitZmienna2 = true; // flaga
+                                string specialChar = @"\|!#$%&/()=?»«@£§€{}.-;'<>_,*+`~ąśćźżółęń";
+
+                                char firstLetter = pomocniczaIndexNazwaZmiennej.FirstOrDefault();
+
+                                if (Char.IsDigit(firstLetter))
+                                {
+                                    czyGitZmienna2 = false;
+                                }
+                                else
+                                {
+                                    foreach (var item in specialChar)
+                                    {
+                                        if (pomocniczaIndexNazwaZmiennej.Contains(item))
+                                        {
+                                            czyGitZmienna2 = false;
+                                        }
+                                    }
+                                }
+                                
+                                if (czyGitZmienna2 == true)
+                                {
+                                    switch (pomocniczaIndexNazwaTypu) //przekazywanie do metody funkcji
+                                    {
+                                        case "ak47":
+                                            funkcje.InterpretujReadLine(subs, subs.Length); //i dalej tutaj będzie robione
+                                            break;
+                                        case "m4a1s":
+                                            funkcje.InterpretujReadLine(subs, subs.Length);
+                                            break;
+                                        case "m4a4":
+                                            funkcje.InterpretujWrite(tempArray, subs.Length);
+                                            break;
+                                        case "usp":
+                                            funkcje.InterpretujToString(tempArray, subs.Length);
+                                            break;
+                                        case "glock":
+                                            funkcje.InterpretujToInt(tempArray, subs.Length);
+                                            break;
+                                        case "tec":
+                                            funkcje.InterpretujToFloat(tempArray, subs.Length);
+                                            break;
+                                        default:
+                                            label4.Text = "Taka funkcja nie istnieje";
+                                            break;
+
+                                    }
+                                    label4.Text = pomocniczaIndexNazwaTypu + " " + pomocniczaIndexNazwaZmiennej;
+                                }
+                                else
+                                {
+                                    label4.Text = "Zła nazwa zmiennej";
+                                }
+                            }
+                        }
+                        else
+                        {
+                            label4.Text = "Źle wpisana funkcja";//komunikat
+                        }
+                    }
+
+
+
                     foreach (var sub in subs)
                     {
                         listView2.Items.Add(sub);
---
+++ ProjektSSIW/Interpreter/Funkcje.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektSSIW.Interpreter
+{
+    public class Funkcje
+    {
+        Składnia składnia = new Składnia();
+
+        public void InterpretujFunkcje(string[] tempArray)
+        {
+            
+
+            for (int i = 1; i < tempArray.Length; i++)
+            {
+                string[] tab = tempArray[i].Split(' ');
+
+                switch (tab[0])
+                {
+                    case "ak47":
+                        InterpretujReadLine(tempArray, i);
+                        break;
+                    case "m4a1s":
+                        InterpretujWriteLine(tempArray, i);
+                        break;
+                    case "m4a4":
+                        InterpretujWrite(tempArray, i);
+                        break;
+                    case "usp":
+                        InterpretujToString(tempArray, i);
+                        break;
+                    case "glock":
+                        InterpretujToInt(tempArray, i);
+                        break;
+                    case "tec":
+                        InterpretujToFloat(tempArray, i);
+                        break;
+                }
+            }
+
+            
+        }
+
+        public void InterpretujReadLine(string[] tempArray, object i)
+        {
+
+            
+
+
+        }
+
+        public void InterpretujToFloat(string[] tempArray, int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InterpretujToInt(string[] tempArray, int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InterpretujToString(string[] tempArray, int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InterpretujWrite(string[] tempArray, int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InterpretujWriteLine(string[] tempArray, object i)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
